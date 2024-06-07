bb72205fccefcfcd9dbd1874f7f27aca9eeb6f48
Steve Martin <Xenomorphxx131@gmail.com>
2024-06-07 15:39:21 -0400

    Passed in variant from the EZ gui file instead of baking in to the driver.

--- Python/EZ_DC2038A.py
+++ Python/EZ_DC2038A.py
@@ -4,8 +4,8 @@ from PyICe.lab_utils.banners import print_banner
 from DC2038A import DC2038A
 from PyICe import lab_core
 
-print_banner("", "Please enter the COM port number for your AQ48 Demo Board.", "It can be obtained from the Windows Device Manager.", "")
+print_banner("Please enter the COM port number for your DC2038A Demo Board.", "It can be obtained from the Windows Device Manager.")
 comport = input("Enter COM port number, COM")
 master = lab_core.channel_master()
-master.add(DC2038A(master, comport=f"COM{comport}", verbose=False))
+master.add(DC2038A(master, comport=f"COM{comport}", variant_file="LTC4162-LADM.xml", verbose=False))
 master.gui()
